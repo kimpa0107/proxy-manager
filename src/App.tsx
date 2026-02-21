768f2093ebc04ea38e6e01793af9f9945ba42f86
JINBO <kimpa0107@163.com>
2026-02-21 12:47:53 +0800

    fix: update port input placeholder from 1080 to 1082

--- src/App.tsx
+++ src/App.tsx
@@ -429,7 +429,7 @@ export default function App() {
                 value={port}
                 onChange={onInputChange}
                 onSubmit={onSave}
-                placeholder='1080'
+                placeholder='1082'
                 disabled={loading}
               />
               
